bb4e0b21244e84d1e4b4d2d78387d65efbb6ca0d
Anuj Gupta <anujgupta@cyient.com>
2025-01-04 21:46:05 +0530

    integration tests

--- spring-integration-tests/src/main/java/com/example/demo/SpringIntegrationTestsApplication.java
+++ spring-integration-tests/src/main/java/com/example/demo/SpringIntegrationTestsApplication.java
@@ -2,28 +2,171 @@ package com.example.demo;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @SpringBootApplication
 public class SpringIntegrationTestsApplication {
 
-	@Autowired
-	private UserRepository repository;
+    @Autowired
+    private UserRepository repository;
 
-	@PostConstruct
-	public void init() {
-		List<UserModel> users = Arrays.asList(
-			new UserModel("anujgupta", "IT", 52000));
-		repository.saveAll(users);
+    @PostConstruct
+    public void init() {
+	List<UserModel> users = Arrays.asList(new UserModel("anujgupta", "IT", 52000));
+	repository.saveAll(users);
+    }
+
+    public static void main(String[] args) {
+	SpringApplication.run(SpringIntegrationTestsApplication.class, args);
+    }
+
+}
+
+@RestController
+class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping("/createUser")
+    public String createUser(@RequestBody UserModel user) {
+	repository.save(user);
+	return "User saved with id: " + user.getId();
+    }
+
+    @GetMapping("/readAllUsers")
+    public List<UserModel> readAllUsers() {
+	return repository.findAll();
+    }
+
+    @GetMapping("/readUserById/{id}")
+    public UserModel readUserById(@PathVariable Integer id) {
+	Optional<UserModel> user = repository.findById(id);
+	if (user.isPresent()) {
+	    return user.get();
+	} else {
+	    throw new RuntimeException("Invalid User Id: " + id);
+	}
+    }
+
+    @GetMapping("/readUserByDept/{dept}")
+    public List<UserModel> readUserById(@PathVariable String dept) {
+	List<UserModel> employees = repository.findByDept(dept);
+	return employees;
+    }
+
+    @GetMapping("/readUserBetweenSalary/{fromSalary}/{toSalary}")
+    public List<UserModel> readUserBetweenSalary(@PathVariable Integer fromSalary, @PathVariable Integer toSalary) {
+	List<UserModel> employees = repository.findBetweenSalary(fromSalary, toSalary);
+	return employees;
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUserById(@RequestBody UserModel employeeInput) {
+	Optional<UserModel> user = repository.findById(employeeInput.getId());
+	if (user.isPresent()) {
+	    repository.save(employeeInput);
+	    return "User Updated Successfully";
+	} else {
+	    throw new RuntimeException("Invalid User Id: " + employeeInput.getId());
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringIntegrationTestsApplication.class, args);
-	}
+    }
+
+    @DeleteMapping("/deleteUserById/{id}")
+    public String updateUserById(@PathVariable Integer id) {
+	repository.deleteById(id);
+	return "User deleted with id: " + id;
+    }
+}
+
+interface UserRepository extends JpaRepository<UserModel, Integer> {
+
+    Optional<UserModel> findById(Integer id);
+
+    List<UserModel> findByDept(String dept);
+
+    @Query(value = "select * from cmn_user where salary between ?1 and ?2", nativeQuery = true)
+    List<UserModel> findBetweenSalary(Integer fromSalary, Integer toSalary);
+
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+	return "Hello " + name;
+    }
+
+    @GetMapping("/employee")
+    public EmployeeModel employee() {
+	return new EmployeeModel(1, "Anuj", 36, new SalaryModel(50000, "GBP"));
+    }
+}
+
+@Data
+@AllArgsConstructor
+class EmployeeModel {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private SalaryModel salaryModel;
+
+}
+
+@Data
+@AllArgsConstructor
+class SalaryModel {
+
+    private Integer salary;
+    private String currency;
+
+}
+
+@Data
+@Entity
+@Table(name = "CMN_USER")
+class UserModel {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String username;
+    private String dept;
+    private Integer salary;
+
+    public UserModel() {
+	super();
+    }
+
+    public UserModel(String username, String dept, Integer salary) {
+	super();
+	this.username = username;
+	this.dept = dept;
+	this.salary = salary;
+    }
 
 }
--- spring-integration-tests/src/test/java/com/example/demo/SpringIntegrationTestsApplicationTests.java
+++ spring-integration-tests/src/test/java/com/example/demo/SpringIntegrationTestsApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-
 import javax.transaction.Transactional;
 
 import org.assertj.core.api.Assertions;
@@ -24,146 +23,142 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional // so that each test case will rut in its own transaction and changes will be
-				// rollbacked after it finishes
+	       // rollbacked after it finishes
 class SpringIntegrationTestsApplicationTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	void deleteUserByIdTest() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.delete("/deleteUserById/1"))
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.content().string("User deleted with id: 1"));
-		
-		try {
-			ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/readUserById/1"));
-			System.out.println(resultActions);
-		} catch (Exception e) {
-			String msg = e.getMessage();
-			Assertions.assertThat(msg).endsWith("Invalid User Id: 1");
-		}
-		
-	}
-	
-	@Test
-	void updateUserTest() throws Exception {
-		ObjectMapper objectMapper = new ObjectMapper();
-		UserModel user = new UserModel("anujguptanew", "MISC", 35000);
-		user.setId(1);
-		mockMvc.perform(MockMvcRequestBuilders.post("/updateUser")
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(user)));
-		mockMvc.perform(MockMvcRequestBuilders.get("/readUserById/1"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.username",CoreMatchers.is("anujguptanew")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.dept",CoreMatchers.is("MISC")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.salary",CoreMatchers.is(35000)));
-		
-	}
-	
-	@Test
-	void readUserBetweenSalaryTest() throws Exception {
-		ObjectMapper objectMapper = new ObjectMapper();
-		mockMvc.perform(MockMvcRequestBuilders.post("/createUser")
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(new UserModel("meethi", "MISC", 30000))));
-		mockMvc.perform(MockMvcRequestBuilders.post("/createUser")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(new UserModel("charu", "MISC", 40000))));
-		mockMvc.perform(MockMvcRequestBuilders.post("/createUser")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(new UserModel("shivang", "MISC", 60000))));
-		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/readUserBetweenSalary/35000/55000"))
-			.andDo(MockMvcResultHandlers.print());
+    @Test
+    void deleteUserByIdTest() throws Exception {
+	mockMvc.perform(MockMvcRequestBuilders.delete("/deleteUserById/1"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.content().string("User deleted with id: 1"));
 
-		UserModel[] responseUsers = objectMapper.readValue(resultActions.andReturn().getResponse().getContentAsString(),
-				UserModel[].class);
-		org.junit.jupiter.api.Assertions.assertEquals(2, responseUsers.length);
-		for (UserModel user : responseUsers) {
-			org.junit.jupiter.api.Assertions.assertTrue(user.getUsername().equals("anujgupta") || user.getUsername().equals("charu"));
-			org.junit.jupiter.api.Assertions.assertTrue(user.getDept().equals("IT") || user.getDept().equals("MISC"));
-			org.junit.jupiter.api.Assertions.assertTrue(user.getSalary().equals(52000) || user.getSalary().equals(40000));
-		}
+	try {
+	    ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/readUserById/1"));
+	    System.out.println(resultActions);
+	} catch (Exception e) {
+	    String msg = e.getMessage();
+	    Assertions.assertThat(msg).endsWith("Invalid User Id: 1");
 	}
-	
-	@Test
-	void readUserByDeptTest() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/readUserByDept/IT"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].id",CoreMatchers.is(1)))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].username",CoreMatchers.is("anujgupta")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].dept",CoreMatchers.is("IT")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].salary",CoreMatchers.is(52000)));
+
+    }
+
+    @Test
+    void updateUserTest() throws Exception {
+	ObjectMapper objectMapper = new ObjectMapper();
+	UserModel user = new UserModel("anujguptanew", "MISC", 35000);
+	user.setId(1);
+	mockMvc.perform(MockMvcRequestBuilders.post("/updateUser").contentType(MediaType.APPLICATION_JSON)
+		.content(objectMapper.writeValueAsString(user)));
+	mockMvc.perform(MockMvcRequestBuilders.get("/readUserById/1"))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.username", CoreMatchers.is("anujguptanew")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.dept", CoreMatchers.is("MISC")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.salary", CoreMatchers.is(35000)));
+
+    }
+
+    @Test
+    void readUserBetweenSalaryTest() throws Exception {
+	ObjectMapper objectMapper = new ObjectMapper();
+	mockMvc.perform(MockMvcRequestBuilders.post("/createUser").contentType(MediaType.APPLICATION_JSON)
+		.content(objectMapper.writeValueAsString(new UserModel("meethi", "MISC", 30000))));
+	mockMvc.perform(MockMvcRequestBuilders.post("/createUser").contentType(MediaType.APPLICATION_JSON)
+		.content(objectMapper.writeValueAsString(new UserModel("charu", "MISC", 40000))));
+	mockMvc.perform(MockMvcRequestBuilders.post("/createUser").contentType(MediaType.APPLICATION_JSON)
+		.content(objectMapper.writeValueAsString(new UserModel("shivang", "MISC", 60000))));
+	ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/readUserBetweenSalary/35000/55000"))
+		.andDo(MockMvcResultHandlers.print());
+
+	UserModel[] responseUsers = objectMapper.readValue(resultActions.andReturn().getResponse().getContentAsString(),
+		UserModel[].class);
+	org.junit.jupiter.api.Assertions.assertEquals(2, responseUsers.length);
+	for (UserModel user : responseUsers) {
+	    org.junit.jupiter.api.Assertions
+		    .assertTrue(user.getUsername().equals("anujgupta") || user.getUsername().equals("charu"));
+	    org.junit.jupiter.api.Assertions.assertTrue(user.getDept().equals("IT") || user.getDept().equals("MISC"));
+	    org.junit.jupiter.api.Assertions
+		    .assertTrue(user.getSalary().equals(52000) || user.getSalary().equals(40000));
 	}
-	
-	@Test
-	void readUserByIdTest() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/readUserById/1"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.id",CoreMatchers.is(1)))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.username",CoreMatchers.is("anujgupta")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.dept",CoreMatchers.is("IT")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.salary",CoreMatchers.is(52000)));
+    }
+
+    @Test
+    void readUserByDeptTest() throws Exception {
+	mockMvc.perform(MockMvcRequestBuilders.get("/readUserByDept/IT"))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].id", CoreMatchers.is(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].username", CoreMatchers.is("anujgupta")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].dept", CoreMatchers.is("IT")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].salary", CoreMatchers.is(52000)));
+    }
+
+    @Test
+    void readUserByIdTest() throws Exception {
+	mockMvc.perform(MockMvcRequestBuilders.get("/readUserById/1"))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.id", CoreMatchers.is(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.username", CoreMatchers.is("anujgupta")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.dept", CoreMatchers.is("IT")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.salary", CoreMatchers.is(52000)));
+    }
+
+    @Test
+    void createUserTest() throws Exception {
+	ObjectMapper objectMapper = new ObjectMapper();
+	MockHttpServletRequestBuilder postRequest = MockMvcRequestBuilders.post("/createUser")
+		.contentType(MediaType.APPLICATION_JSON)
+		.content(objectMapper.writeValueAsString(new UserModel("harsha", "IT", 80000)));
+	ResultActions resultActions = mockMvc.perform(postRequest).andExpect(MockMvcResultMatchers.status().isOk())
+		.andDo(MockMvcResultHandlers.print());
+
+	String response = resultActions.andReturn().getResponse().getContentAsString();
+	Assertions.assertThat(response).startsWith("User saved with id:");
+    }
+
+    @Test
+    void readAllUsersTest1() throws Exception {
+	ObjectMapper objectMapper = new ObjectMapper();
+	mockMvc.perform(MockMvcRequestBuilders.post("/createUser").contentType(MediaType.APPLICATION_JSON)
+		.content(objectMapper.writeValueAsString(new UserModel("harsha", "IT", 80000))));
+	ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/readAllUsers"))
+		.andDo(MockMvcResultHandlers.print());
+	UserModel[] responseUsers = objectMapper.readValue(resultActions.andReturn().getResponse().getContentAsString(),
+		UserModel[].class);
+	System.out.println(responseUsers);
+	org.junit.jupiter.api.Assertions.assertEquals(2, responseUsers.length);
+	for (UserModel user : responseUsers) {
+	    org.junit.jupiter.api.Assertions
+		    .assertTrue(user.getUsername().equals("anujgupta") || user.getUsername().equals("harsha"));
+	    org.junit.jupiter.api.Assertions.assertTrue(user.getDept().equals("IT"));
+	    org.junit.jupiter.api.Assertions
+		    .assertTrue(user.getSalary().equals(52000) || user.getSalary().equals(80000));
 	}
-	
-	@Test
-	void createUserTest() throws Exception {
-		ObjectMapper objectMapper = new ObjectMapper();
-		MockHttpServletRequestBuilder postRequest = MockMvcRequestBuilders.post("/createUser")
-			.contentType(MediaType.APPLICATION_JSON)
-			.content(objectMapper.writeValueAsString(new UserModel("harsha", "IT", 80000)));
-		ResultActions resultActions = mockMvc.perform(postRequest)
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andDo(MockMvcResultHandlers.print());
-		
-		String response = resultActions.andReturn().getResponse().getContentAsString();
-		Assertions.assertThat(response).startsWith("User saved with id:");
-	}
-	
-	@Test
-	void readAllUsersTest1() throws Exception {
-		ObjectMapper objectMapper = new ObjectMapper();
-		mockMvc.perform(MockMvcRequestBuilders.post("/createUser")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(new UserModel("harsha", "IT", 80000))));
-		ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/readAllUsers"))
-			.andDo(MockMvcResultHandlers.print());
-		UserModel[] responseUsers = objectMapper.readValue(resultActions.andReturn().getResponse().getContentAsString(),
-				UserModel[].class);
-		System.out.println(responseUsers);
-		org.junit.jupiter.api.Assertions.assertEquals(2, responseUsers.length);
-		for (UserModel user : responseUsers) {
-			org.junit.jupiter.api.Assertions.assertTrue(user.getUsername().equals("anujgupta") || user.getUsername().equals("harsha"));
-			org.junit.jupiter.api.Assertions.assertTrue(user.getDept().equals("IT"));
-			org.junit.jupiter.api.Assertions.assertTrue(user.getSalary().equals(52000) || user.getSalary().equals(80000));
-		}
-	}
-	
-	@Test
-	void readAllUsersTest() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/readAllUsers"))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].id",CoreMatchers.is(1)))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].username",CoreMatchers.is("anujgupta")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].dept",CoreMatchers.is("IT")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$[0].salary",CoreMatchers.is(52000)));
-	}
-	
-	@Test
-	void employeeTest() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/employee"))
-			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.jsonPath("$.id",CoreMatchers.is(1)))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.name",CoreMatchers.is("Anuj")))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.age",CoreMatchers.is(36)))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.salaryModel.salary",CoreMatchers.is(50000)))
-			.andExpect(MockMvcResultMatchers.jsonPath("$.salaryModel.currency",CoreMatchers.is("GBP")));
-	}
-	
-	@Test
-	void helloTest() throws Exception {
-		RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/hello/anuj");
-		ResultMatcher matcher = MockMvcResultMatchers.status().isOk();
-		mockMvc.perform(requestBuilder)
-			.andExpect(matcher)
-			.andExpect(MockMvcResultMatchers.content().string("Hello anuj"));
-	}
+    }
+
+    @Test
+    void readAllUsersTest() throws Exception {
+	mockMvc.perform(MockMvcRequestBuilders.get("/readAllUsers"))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].id", CoreMatchers.is(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].username", CoreMatchers.is("anujgupta")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].dept", CoreMatchers.is("IT")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].salary", CoreMatchers.is(52000)));
+    }
+
+    @Test
+    void employeeTest() throws Exception {
+	mockMvc.perform(MockMvcRequestBuilders.get("/employee")).andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.id", CoreMatchers.is(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", CoreMatchers.is("Anuj")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.age", CoreMatchers.is(36)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.salaryModel.salary", CoreMatchers.is(50000)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.salaryModel.currency", CoreMatchers.is("GBP")));
+    }
+
+    @Test
+    void helloTest() throws Exception {
+	RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/hello/anuj");
+	ResultMatcher matcher = MockMvcResultMatchers.status().isOk();
+	mockMvc.perform(requestBuilder).andExpect(matcher)
+		.andExpect(MockMvcResultMatchers.content().string("Hello anuj"));
+    }
 
 }
